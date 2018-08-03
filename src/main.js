0baa6ba4212dcef61b20fe55239438c6230e0b21
chentian <326932483@qq.com>
2018-08-03 16:32:20 +0800

    [dev]-底部tab实现

--- src/main.js
+++ src/main.js
@@ -14,9 +14,30 @@ export default {
     pages: ['pages/logs/main', '^pages/index/main'],
     window: {
       backgroundTextStyle: 'light',
-      navigationBarBackgroundColor: '#fff',
-      navigationBarTitleText: 'WeChat',
-      navigationBarTextStyle: 'black'
+      navigationBarBackgroundColor: '#9BCD9B',
+      navigationBarTitleText: 'novel',
+      navigationBarTextStyle: 'white'
+    },
+    tabBar: {
+      color: '#fff',
+      selectedColor: '#006b4c',
+      backgroundColor: '#9BCD9B',
+      list: [{
+        pagePath: 'pages/index/main',
+        text: '首页',
+        iconPath: '/static/images/home.png',
+        selectedIconPath: '/static/images/home-active.png'
+      }, {
+        pagePath: 'pages/rank/main',
+        text: '排行',
+        iconPath: '/static/images/rank.png',
+        selectedIconPath: '/static/images/rank-active.png'
+      }, {
+        pagePath: 'pages/user/main',
+        text: '个人中心',
+        iconPath: '/static/images/user.png',
+        selectedIconPath: '/static/images/user-active.png'
+      }]
     }
   }
 }
